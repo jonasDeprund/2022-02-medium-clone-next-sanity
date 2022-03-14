316de38675f461bfacbca18b2b439dd323203e30
jonas <jonasdeprund@gmail.com>
2022-03-14 16:59:17 +0100

    bugs fixe

--- sanity.js
+++ sanity.js
@@ -1,10 +1,6 @@
 import createImageUrlBuilder from '@sanity/image-url'
 
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient,
-} from 'next-sanity'
+import { createCurrentUserHook, createClient } from 'next-sanity'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
