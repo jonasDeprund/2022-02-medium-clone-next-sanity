c4595caf4dd491d6721cbc3232819aa3966e8635
jonas <jonasdeprund@gmail.com>
2022-03-30 15:15:40 +0200

    form wip

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,12 +3,26 @@ import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
   return (
     <main>
       <Header />
@@ -43,7 +57,57 @@ function Post({ post }: Props) {
           />
         </div>
       </article>
-      <hr />
+      <hr className="mx-auto max-w-lg border border-yellow-500" />
+
+      <input {...register('_id')} type="hidden" name="_id" value={post._id} />
+
+      <form className="mx-auto mb-10 flex max-w-2xl flex-col p-5 ">
+        <label className="mb-5 block">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register('name', { required: true })}
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none focus:ring"
+            placeholder="John Doe"
+            type="text"
+          />
+        </label>
+
+        <label className="mb-5 block">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register('email', { required: true })}
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none focus:ring"
+            placeholder="John Doe"
+            type="text"
+          />
+        </label>
+
+        <label className="mb-5 block">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register('comment', { required: true })}
+            className="form-input form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none focus:ring"
+            placeholder="John Doe"
+            rows={8}
+          />
+        </label>
+        {}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">The name field is required</span>
+          )}{' '}
+          {errors.comment && (
+            <span className="text-red-500">The comment field is required</span>
+          )}{' '}
+          {errors.email && (
+            <span className="text-red-500">The email field is required</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="focus:shadow-outline rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+        />
+      </form>
     </main>
   )
 }
