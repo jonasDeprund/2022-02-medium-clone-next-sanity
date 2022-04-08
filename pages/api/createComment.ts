223027a858fd04814d39e7eefbf2e4f0da2db6c3
jonas <jonasdeprund@gmail.com>
2022-04-08 18:15:54 +0200

    project done

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { SanityClient } from '@sanity/client'
+import { sanityClient } from '../../sanity'
 
 const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
